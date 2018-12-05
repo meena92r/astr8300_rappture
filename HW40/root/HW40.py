c40dab3f164a5b1db39c62c16ea3989fdf166a14
Ubuntu <ubuntu@ip-172-31-88-34.ec2.internal>
2018-12-05 01:10:51 +0000

    new file:   HW40/root/HW40.py 	new file:   HW40/root/tool.xml

---
+++ HW40/root/HW40.py
@@ -0,0 +1,80 @@
+#import argparse
+import Rappture
+import sys
+from math import *
+import numpy as np
+#import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+from scipy import optimize
+
+def f(y, x, b, formula):
+    return eval(formula)
+
+def calc(x, a, formula):
+    y = odeint(f, -a, [0,x], args=(1, formula))
+    return y[1]  # y[0] is int f(x) from 0 to 0
+
+def main():
+
+    # Get arguments
+
+#    parser = argparse.ArgumentParser(description='Find root')
+#    parser.add_argument('formula', type=str, help='f(x)')
+#    parser.add_argument('--xmin', type=float, default = 0, help='min x')
+#    parser.add_argument('--xmax', type=float, default = 100, help='max x')
+#    parser.add_argument('--a', type=float, default = 10, help='a')
+#    args = parser.parse_args()
+
+
+    # Getting arguments with Rappture
+
+     io = Rappture.library(sys.argv[1])
+     xmin = float(io.get('input.number(min).current'))
+     xmax = float(io.get('input.number(max).current'))
+     y0 = float(io.get('input.number(Y0).current'))
+     npts = int(io.get('input.number(Npts).current'))
+     formula = io.get('input.string(formula).current')
+      
+     
+
+    # Get base string
+
+#    my_str_base = 'int_0^root (' + args.formula + ') dx - ' + str(args.a)
+
+     my_str_base = 'int_0^root (' + formula + ') dx - ' + str(npts)
+
+    # Get compute root of \int_0^x f(x') dx' - a
+
+ #   root = optimize.brentq(
+  #             calc, args.xmin, args.xmax, args=(args.a, args.formula)
+  #         )
+
+     root = optimize.brentq(
+               calc, xmin, xmax, args=(npts, formula)
+           )
+
+  #  my_str = '\nRoot of ' + my_str_base +  ' in [' + str(args.xmin) + \
+   #         ', ' + str(args.xmax) + '] is ' + str(root)
+
+     my_str = '\nRoot of' + my_str_base + ' in [' + str(xmin) + ' to ' + \
+            str(xmax) + ' is ' + str(root)
+    
+#    print(my_str)
+
+    # Check
+
+#    check = calc(root, args.a, args.formula)
+
+#    my_str2 = '\nCheck: ' + my_str_base + ' = ' + str(check[0])
+
+#    print(my_str2 + '\n')
+
+     io.put('output.string(result2).about.label', 'Root')
+     io.put('output.string(result2).current', my_str)
+
+     Rappture.result(io)
+
+
+if __name__ == "__main__":
+    main()
+
